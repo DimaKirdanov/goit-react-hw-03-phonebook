2c45191181ee1345da9e1c3013e23ee797511558
DimaKirdanov <108092906+DimaKirdanov@users.noreply.github.com>
2023-01-25 21:24:30 +0200

    Initial commit

---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './ContactList.module.css';
+const ContactList = ({ filter, onDeleteContact }) => {
+  return (
+    <ul className={styles.list}>
+      {filter.map(({ id, name, number }) => (
+        <li className={styles.item} key={id}>
+          <p className={styles.text}>
+            {name}: {number}
+          </p>
+          <button
+            className={styles.btnDelete}
+            type="button"
+            onClick={() => onDeleteContact(id)}
+          >
+            Delete
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+};
+ContactList.prototype = {
+  filter: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      number: PropTypes.number.isRequired,
+    })
+  ),
+  onDeleteContact: PropTypes.func.isRequired,
+};
+export default ContactList;
